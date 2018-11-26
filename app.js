1ea9a64daec29122635196e76e3c2da9dadc9793
Tige <ecolnago@yahoo.com>
2018-11-26 01:26:33 +0000

    added mongo lab

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ var shopRoutes = require('./routes/shop'),
     indexRoutes = require('./routes/index');
     
 //set database connection    
-mongoose.connect('mongodb://localhost/pma', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost/pma', { useNewUrlParser: true });//local
+mongoose.connect('mongodb://tige:FatFrank37@ds163656.mlab.com:63656/pma', { useNewUrlParser: true });//mlab sandbox
+
 //use body parser 
 app.use(bodyParser.urlencoded({extended: true}));
 //use ejs
