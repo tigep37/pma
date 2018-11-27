0ab7e6f36003f84368641b99e36fe4b6b3b2e149
Tige <ecolnago@yahoo.com>
2018-11-27 04:17:18 +0000

    updated db path for heroku

--- app.js
+++ app.js
@@ -15,9 +15,8 @@ var shopRoutes = require('./routes/shop'),
     indexRoutes = require('./routes/index');
     
 //set database connection 
-//mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
-mongoose.connect('mongodb://localhost/pma', { useNewUrlParser: true });//local
-//mongoose.connect('mongodb://tige:FatFrank37@ds163656.mlab.com:63656/pma', { useNewUrlParser: true });//mlab sandbox
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost/pma', { useNewUrlParser: true });//local
 
 //use body parser 
 app.use(bodyParser.urlencoded({extended: true}));
