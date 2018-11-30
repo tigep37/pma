56f3399e0acda3d73023c369976014d1f618b3bf
Tige <ecolnago@yahoo.com>
2018-11-30 23:05:21 +0000

    added user profile and related files

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,5 @@
 var Item  = require('../models/shop');
+var User  = require('../models/user');
 
 var middlewareOjb = {};
 
@@ -33,5 +34,26 @@ middlewareOjb.checkItemOwnership = function(req, res, next){
     }
 };
 
+//checks ownership of user profile record
+middlewareOjb.checkUserProfile = function(req, res, next){
+    if(req.isAuthenticated()){
+         User.findById(req.params.id, function(err, foundUser){
+           if(err){
+               req.flash('error', 'User not found');
+               res.redirect('back');
+           }else{
+               if(foundUser._id.equals(req.user._id) || req.user.isAdmin){
+                   next();
+               }else{
+                   req.flash('error', 'You do not have permission to do that.');
+                   res.redirect('back');
+               }
+           }
+        });       
+    }else{
+        req.flash('error', 'You need to log in to do that.');
+        res.redirect('back');
+    }
+};
 
 module.exports = middlewareOjb;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express     = require('express'),
     router      = express.Router(),
     passport    = require('passport'),
-    User        = require('../models/user');
+    User        = require('../models/user'),
+    middleware  = require('../middleware');
     
 //routes
 //landing page
@@ -34,6 +35,43 @@ router.post('/register', function(req, res) {
         });
     });
 });
+
+//show user profile
+router.get('/user/:id', function(req, res) {
+   User.findOne({ _id: req.params.id}, function(err, foundUser){
+       if(err){
+           req.flash('error', 'No user record found');
+           res.redirect('/');
+       }else{
+           res.render('user/show', { user: foundUser, page: 'profile' });
+       }
+   }) 
+});
+
+//edit user profile page
+router.get('/user/:id/edit', function(req, res) {
+    User.findOne({ _id: req.params.id}, function(err, foundUser){
+        if(err){
+            req.flash('error', 'something went wrong, you probably should not go there');
+            res.redirect('user/show', { user: req.user, page: 'profile'});
+        }else{
+            res.render('user/edit', { user: foundUser, page: 'profile' });
+        }
+    }) 
+});
+
+//process profile change
+router.put('/user/:id', middleware.checkUserProfile, function(req, res){
+    User.findByIdAndUpdate(req.params.id, req.body.user, function(err, updatedUser){
+        if(err){
+            res.redirect('back');
+        }else{
+            req.flash('success', 'Profile successfully updated!')
+            res.redirect('/user/' + req.params.id);
+        }
+    });
+});
+
 
 //login page
 router.get('/login', function(req, res) {
